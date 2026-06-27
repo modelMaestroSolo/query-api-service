5f580db51023b325b7d7657e50868134856910e9
Solomon Yeboah <modelmaestrosolo@gmail.com>
2026-06-27 01:21:13 +0000

    Bootstrap app

---
+++ src/main/java/com/fileloaderservice/queryapi/QueryapiApplication.java
@@ -0,0 +1,13 @@
+package com.fileloaderservice.queryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fileloaderservice/queryapi/QueryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileloaderservice.queryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
